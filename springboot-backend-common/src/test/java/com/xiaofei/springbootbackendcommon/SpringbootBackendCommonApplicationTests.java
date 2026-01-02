12a8438f719cecd8d7ec785f59b9f02fb1428bb1
tuaofei <tu_aofei@163.com>
2026-01-02 11:51:45 +0800

    更新配置

---
+++ springboot-backend-common/src/test/java/com/xiaofei/springbootbackendcommon/SpringbootBackendCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaofei.springbootbackendcommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBackendCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ springboot-backend-fileupload/src/test/java/com/xiaofei/springbootbackendfileupload/SpringbootBackendFileuploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaofei.springbootbackendfileupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBackendFileuploadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
